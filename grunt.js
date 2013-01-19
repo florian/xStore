a33f68068aab6000b256d6ea3a4733d78238fa22
Florian H <jscoderr@gmail.com>
2013-01-19 15:55:56 +0100

    Add min task

--- grunt.js
+++ grunt.js
@@ -2,6 +2,17 @@ module.exports = function (grunt) {
 
 	grunt.initConfig({
 
+		meta: {
+			banner: '// ' + grunt.file.read('src/loStorage.js').split("\n")[0]
+		},
+
+		min: {
+			dist: {
+				src: ['<banner>', 'src/loStorage.js'],
+				dest: 'src/loStorage.min.js'
+			}
+		},
+
 		jasmine: {
 			all: ['spec/index.html']
 		},
@@ -16,6 +27,9 @@ module.exports = function (grunt) {
 	});
 
 	grunt.loadNpmTasks('grunt-jasmine-task');
+
 	grunt.registerTask('test', 'jasmine');
+	grunt.registerTask('release', 'test min');
+	grunt.registerTask('default', 'release');
 
 };
