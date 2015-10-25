8c6731e517cbad408b4c36c159fe6c15ba28c4a4
Florian Hartmann <jscoderr@gmail.com>
2015-10-25 10:50:40 +0100

    Add grunt wait task

--- Gruntfile.js
+++ Gruntfile.js
@@ -36,6 +36,7 @@ module.exports = function (grunt) {
 
 	});
 
+	grunt.loadNpmTasks('grunt-contrib-watch');
 	grunt.loadNpmTasks('grunt-contrib-jasmine');
 	grunt.loadNpmTasks('grunt-contrib-uglify');
 
