1237336bc5c4833b8216f2ff99746fd81cb040b4
Florian Hartmann <jscoderr@gmail.com>
2016-09-24 14:36:55 +0200

    New spec for v2

--- spec/spec.js
+++ spec/spec.js
@@ -1,8 +1,11 @@
 var parse = JSON.parse;
 
-describe('storage', function () {
+var store = new xStore('', localStorage);
+var session = new xStore('', sessionStorage);
+
+describe('xStore localStorage', function () {
 	beforeEach(function () {
-		storage.empty();
+		store.empty();
 
 		jasmine.addMatchers({
 			toHaveLocalValue: function (utils, customEqualityTesters) {
@@ -21,17 +24,17 @@ describe('storage', function () {
 	});
 
 	it('should be in the global scope', function () {
-		expect(storage).toBeDefined();
+		expect(xStore).toBeDefined();
 	});
 
 	describe('set', function () {
 		it('should be able to set a single value', function () {
-			storage.set('a', 'b');
+			store.set('a', 'b');
 			expect('a').toHaveLocalValue('b');
 		});
 
 		it('should be able to set multiple values', function () {
-			storage.set({
+			store.set({
 				a: 'a',
 				b: 'b'
 			});
@@ -41,7 +44,7 @@ describe('storage', function () {
 		});
 
 		it('should be able to set booleans', function () {
-			storage.set({
+			store.set({
 				a: true,
 				b: false
 			});
@@ -51,7 +54,7 @@ describe('storage', function () {
 		});
 
 		it('should be able to set numbers', function () {
-			storage.set({
+			store.set({
 				int: 42,
 				negative: -42,
 				float: 1.5
@@ -63,62 +66,68 @@ describe('storage', function () {
 		});
 
 		it('should be able to set arrays', function () {
-			storage.set('array', [1, 2, 3]);
+			store.set('array', [1, 2, 3]);
 			expect('array').toHaveLocalValue([1, 2, 3]);
 		});
 
 		it('should be able to set plain objects', function () {
-			storage.set('obj', {a: 1, b: 2});
+			store.set('obj', {a: 1, b: 2});
 			expect('obj').toHaveLocalValue({a: 1, b: 2});
 		});
 
-		it('should return the storage object', function () {
-			expect(storage.set('a', 'b')).toBe(storage);
+		it('should return the store object', function () {
+			expect(store.set('a', 'b')).toBe(store);
+		});
+
+		it('should have the correct prefix', function () {
+		  var prefixStore = new xStore('prefix:', localStorage);
+		  prefixStore.set('a', 'b');
+		  expect(store.get('prefix:a')).toBe('b');
 		});
 	});
 
 	describe('get', function () {
 		it('should return stored value', function () {
-			storage.set('a', 'b');
-			expect(storage.get('a')).toEqual('b');
+			store.set('a', 'b');
+			expect(store.get('a')).toEqual('b');
 		});
 
 		it('should return the fallback for undefined keys', function () {
-			expect(storage.get('x', 'fallback')).toEqual('fallback');
+			expect(store.get('x', 'fallback')).toEqual('fallback');
 		});
 
 		it('should return an object with the correct values when passing in an array', function () {
-			storage.set('a', 1).set('b', 2);
-			expect(storage.get(['a', 'b'])).toEqual({a: 1, b: 2});
+			store.set('a', 1).set('b', 2);
+			expect(store.get(['a', 'b'])).toEqual({a: 1, b: 2});
 		});
 
 		it('should accept fallbacks when passing in an array', function () {
-			storage.set('a', 1);
-			expect(storage.get(['a', 'b'], 42)).toEqual({a: 1, b: 42});
+			store.set('a', 1);
+			expect(store.get(['a', 'b'], 42)).toEqual({a: 1, b: 42});
 		});
 	});
 
 	describe('all', function () {
 		it('should return all available values as an object', function () {
-			storage.set('a', 1).set('b', 2);
-			expect(storage.all()).toEqual({a: 1, b: 2});
+			store.set('a', 1).set('b', 2);
+			expect(store.all()).toEqual({a: 1, b: 2});
 		});
 	});
 
 	describe('remove', function () {
 		it('should be able to remove a single key', function () {
-			storage.set('a', 1).remove('a');
+			store.set('a', 1).remove('a');
 			expect('a' in localStorage).toBe(false);
 		});
 
 		it('should be able to remove several keys', function () {
-			storage.set('a', 1).set('b', 2).remove('a', 'b');
+			store.set('a', 1).set('b', 2).remove('a', 'b');
 			expect('a' in localStorage).toBe(false);
 			expect('b' in localStorage).toBe(false);
 		});
 
 		it('should be able to remove several keys in an array', function () {
-			storage.set('a', 1).set('b', 2).remove(['a', 'b']);
+			store.set('a', 1).set('b', 2).remove(['a', 'b']);
 			expect('a' in localStorage).toBe(false);
 			expect('b' in localStorage).toBe(false);
 		});
@@ -126,124 +135,120 @@ describe('storage', function () {
 
 	describe('empty', function () {
 		it('should remove all values', function () {
-			storage.set('a', 1).set('b', 2).empty();
+			store.set('a', 1).set('b', 2).empty();
 			expect(localStorage.length).toBe(0);
 		});
 
 		it('should return the parent object', function () {
-			expect(storage.empty()).toBe(storage);
+			expect(store.empty()).toBe(store);
 		});
 	});
 
 	describe('invert', function () {
 		it('should invert the value', function () {
-			storage.set('bool', true).invert('bool');
+			store.set('bool', true).invert('bool');
 			expect('bool').toHaveLocalValue(false);
 		});
 
 		it('should return the parent object', function () {
-			expect(storage.invert('key')).toBe(storage);
+			expect(store.invert('key')).toBe(store);
 		});
 	});
 
 	describe('add', function () {
 		it('should add to the value', function () {
-			storage.set('n', 40).add('n', 2);
+			store.set('n', 40).add('n', 2);
 			expect('n').toHaveLocalValue(42);
 		});
 
 		it('should return the parent object', function () {
-			expect(storage.add('key', 5)).toBe(storage);
+			expect(store.add('key', 5)).toBe(store);
 		});
 	});
 
 	describe('increase', function () {
 		it('should increase the value by 1', function () {
-			storage.set('n', 40).increase('n');
+			store.set('n', 40).increase('n');
 			expect('n').toHaveLocalValue(41);
 		});
 
 		it('should accept a value to increase by', function () {
-			storage.set('n', 40).increase('n', 2);
+			store.set('n', 40).increase('n', 2);
 			expect('n').toHaveLocalValue(42);
 		});
 
 		it('should return the parent object', function () {
-			expect(storage.increase('key', 5)).toBe(storage);
+			expect(store.increase('key', 5)).toBe(store);
 		});
 	});
 
 	describe('decrease', function () {
 		it('should decrease the value by 1', function () {
-			storage.set('n', 40).decrease('n');
+			store.set('n', 40).decrease('n');
 			expect('n').toHaveLocalValue(39);
 		});
 
 		it('should accept a value to decrease by', function () {
-			storage.set('n', 40).decrease('n', 2);
+			store.set('n', 40).decrease('n', 2);
 			expect('n').toHaveLocalValue(38);
 		});
 
 		it('should return the parent object', function () {
-			expect(storage.decrease('key', 5)).toBe(storage);
+			expect(store.decrease('key', 5)).toBe(store);
 		});
 	});
 
 	describe('concat', function () {
 		it('should concat the value', function () {
-			storage.set('str', 'Hello').concat('str', ' World');
+			store.set('str', 'Hello').concat('str', ' World');
 			expect('str').toHaveLocalValue('Hello World');
 		});
 
 		it('should return the parent object', function () {
-			expect(storage.concat('key', 'str')).toBe(storage);
+			expect(store.concat('key', 'str')).toBe(store);
 		});
 	});
 
 	describe('push', function () {
 		it('should push the value', function () {
-			storage.set('arr', [1, 2]).push('arr', 3, 4);
+			store.set('arr', [1, 2]).push('arr', 3, 4);
 			expect('arr').toHaveLocalValue([1, 2, 3, 4]);
 		});
 
 		it("should create the array and push if it doesn't exist", function () {
-			storage.push('list', 1, 2);
+			store.push('list', 1, 2);
 			expect('list').toHaveLocalValue([1, 2]);
 		});
 
 		it('should return the parent object', function () {
-			expect(storage.push('key', 1)).toBe(storage);
+			expect(store.push('key', 1)).toBe(store);
 		});
 	});
 
 	describe('extend', function () {
 		it('should extend the object with the key and value', function () {
-			storage.set('obj', {a: 1}).extend('obj', 'b', 2);
+			store.set('obj', {a: 1}).extend('obj', 'b', 2);
 			expect('obj').toHaveLocalValue({a: 1, b: 2});
 		});
 
 		it('should be able to extend with an object', function () {
-			storage.set('obj', {a: 1}).extend('obj', {b: 2});
+			store.set('obj', {a: 1}).extend('obj', {b: 2});
 			expect('obj').toHaveLocalValue({a: 1, b: 2});
 		});
 
 		it("should create the object if it doesn't exist yet", function () {
-			storage.extend('obj', {a: 1});
+			store.extend('obj', {a: 1});
 			expect('obj').toHaveLocalValue({a: 1});
 		});
 
 		it('should return the parent object', function () {
-			expect(storage.extend('key', 'a', 1)).toBe(storage);
+			expect(store.extend('key', 'a', 1)).toBe(store);
 		});
 	});
 });
 
-describe('session', function () {
-	it('should be in the global scope', function () {
-		expect(session).toBeDefined();
-	});
-
-	it('should be operating with sessionStorage', function () {
+describe('xStore sessionStorage', function () {
+	it('should be operating with sessionstore', function () {
 		session.set('a', 'b');
 		expect(parse(sessionStorage.getItem('a'))).toBe('b');
 	});
